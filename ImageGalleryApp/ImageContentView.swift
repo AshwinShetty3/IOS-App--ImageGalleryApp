46849f655059a577244476bbcef1e3213e4b7d22
ashwin <as9932418@gmail.com>
2025-02-15 22:58:05 +0530

    Initial Commit

---
+++ ImageGalleryApp/ImageContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ImageGalleryApp
+//
+//  Created by Ashwin Shetty on 15/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ImageGalleryApp/ImageGalleryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageGalleryAppApp.swift
+//  ImageGalleryApp
+//
+//  Created by Ashwin Shetty on 15/02/25.
+//
+
+import SwiftUI
+
+@main
+struct ImageGalleryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
