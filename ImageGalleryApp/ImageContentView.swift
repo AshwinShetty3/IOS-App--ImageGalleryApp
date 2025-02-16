61adc5ceafebf231f4120ab960e51fced4b9418d
Ashwin shetty <74811626+ashwin12377@users.noreply.github.com>
2025-02-16 10:26:29 +0530

    Update ImageContentView.swift

--- ImageGalleryApp/ImageContentView.swift
+++ ImageGalleryApp/ImageContentView.swift
@@ -7,18 +7,56 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+struct ImageContentView: View {
+    @State private var photos: [Photo] = []
+    @State private var searchText = ""
+    @State private var favourites: [Photo] = []
+    @State private var isLandscape = UIDevice.current.orientation.isLandscape
+
+    var filteredPhotos: [Photo] {
+        if searchText.isEmpty {
+            return photos
+        } else {
+            return photos.filter { $0.description?.lowercased().contains(searchText.lowercased()) ?? false }
         }
-        .padding()
+    }
+
+    var body: some View {
+        NavigationView {
+            
+            ScrollView {
+                        LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: isLandscape ? 20 : 10), count: isLandscape ? 5 : 3), spacing: isLandscape ? 20 : 10) {
+                            ForEach(filteredPhotos) { photo in
+                                NavigationLink(destination: ImageDetailView(photo: photo, favourites: $favourites)) {
+                                    AsyncImage(url: URL(string: photo.urls.small)) { image in
+                                        image.resizable().scaledToFill()
+                                    } placeholder: {
+                                        ProgressView()
+                                    }
+                                    .frame(width: isLandscape ? 120 : 120, height: isLandscape ? 150 : 160)
+                                    .cornerRadius(8)
+                                }
+                            }
+                        }
+                        .padding()
+            }
+            .navigationTitle("Image Gallery")
+            .toolbar {
+                NavigationLink(destination: FavouritesImageView(favourites: $favourites)) {
+                    Image(systemName: "heart.fill")
+                        .foregroundColor(.red)
+                }
+            }
+            .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always))
+        }
+        .onAppear {
+            ImageAPI.shared.fetchPhotos { fetchedPhotos in
+                if let fetchedPhotos = fetchedPhotos {
+                    photos = fetchedPhotos
+                }
+            }
+        }
+
     }
 }
 
-#Preview {
-    ContentView()
-}
