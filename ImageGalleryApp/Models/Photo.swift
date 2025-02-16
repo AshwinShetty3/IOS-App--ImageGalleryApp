2312e62c637ed36940f59b1eda47ff2c412b55d0
Ashwin shetty <74811626+ashwin12377@users.noreply.github.com>
2025-02-16 10:28:16 +0530

    Update Photo.swift

---
+++ ImageGalleryApp/Models/Photo.swift
@@ -0,0 +1,25 @@
+//
+//  Photo.swift
+//  ImageGalleryApp
+//
+//  Created by Ashwin Shetty on 15/02/25.
+//
+
+import Foundation
+
+struct Photo: Codable, Identifiable {
+    let id: String
+    let urls: PhotoURLs
+    let description: String?
+    let altDescription: String?
+    let user: User
+
+    struct PhotoURLs: Codable {
+        let small: String
+        let full: String
+    }
+
+    struct User: Codable {
+        let name: String
+    }
+}
